e581e3cac690bbcc453dd6ff9d3267ad140abe9b
maximdsouzaml <36703574+maximdsouzaml@users.noreply.github.com>
2018-04-07 08:31:52 +0530

    regression

--- linear_regression.py
+++ linear_regression.py
@@ -1,4 +1,5 @@
 import numpy as np
+import matplotlib.pyplot as plt
 
 m = 100
 X = 6 * np.random.rand(m, 1) - 3
@@ -21,10 +22,41 @@ lin_reg.intercept_, lin_reg.coef_
 
 from sklearn.metrics import mean_squared_error
 from sklearn.model_selection import train_test_split
-
+training_error=[]
+val_error=[]
 def plot_learning_curves(model, X, y):
     X_train, X_val, y_train, y_val=train_test_split(X, y, test_size=.2)
     
+    for k in range(1, np.size(X_train)):
+        print(k)
+        model.fit(X_train[:k], y_train[:k])
+        y_pred_train=model.predict(X_train[:k])
+        y_pred_val=model.predict(X_val)
+        training_error.append(mean_squared_error(y_pred_train, y_train[:k]))
+        val_error.append(mean_squared_error(y_pred_val, y_val))
+    plt.plot(np.sqrt(training_error), "r-+", linewidth=2, label="train")
+    plt.plot(np.sqrt(val_error), "b-+", linewidth=2, label="val")
     
-    
+lin_reg = LinearRegression()
+plot_learning_curves(lin_reg, X, y)
+
+
+from sklearn.pipeline import Pipeline
+
+poly_features=Pipeline([
+        ("poly", PolynomialFeatures(degree=3, include_bias=False)),
+        ("linear", LinearRegression()), 
+        ])
+        
+plot_learning_curves(poly_features, X, y)    
+
+
+from sklearn.pipeline import Pipeline
+
+polynomial_regression = Pipeline([
+        ("poly_features", PolynomialFeatures(degree=10, include_bias=False)),
+        ("lin_reg", LinearRegression()),
+    ])
+
+plot_learning_curves(polynomial_regression, X, y)
     
