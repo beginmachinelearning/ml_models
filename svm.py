3668ed1a8455fdb2ea87caad0a976d253769c46b
maximdsouzaml <36703574+maximdsouzaml@users.noreply.github.com>
2018-04-17 19:17:29 +0530

    go

--- svm.py
+++ svm.py
@@ -3,3 +3,5 @@ from sklearn import datasets
 from sklearn.pipeline import Pipeline
 from sklearn.preprocessing import StandardScaler
 from sklearn.svm import LinearSVC
+
+iris_dataset=datasets.load_iris()
